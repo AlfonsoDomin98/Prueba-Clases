b50876b300b3e326a0c39613dc0c03d01a22b3ac
Alfonso <Alfonso@192.168.1.196>
2017-02-06 14:25:55 +0100

    Ejercicios

---
+++ src/Alumno.java
@@ -0,0 +1,12 @@
+
+public class Alumno {
+	
+	public String Nombre;
+	public String Apellido;
+	public int Edad;
+	public int Curso;
+	
+	public void saludo (){
+		System.out.println("Hi Jose");
+	}
+}
---
+++ src/AlumnoTest.java
@@ -0,0 +1,25 @@
+
+public class AlumnoTest {
+
+	public static void main(String[] args) {
+		
+		Alumno alum = new Alumno();
+		
+		Alumno alum1 = new Alumno();
+		
+		Alumno alum2 = new Alumno();
+		
+		
+		alum.Nombre = "Pepe";
+		
+		alum1.Nombre = "Paco";
+		
+		alum2.Nombre = "Ramon";
+		
+	
+		
+		System.out.println(alum.Nombre );
+
+	}
+
+}
